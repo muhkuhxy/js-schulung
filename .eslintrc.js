8aea9a680e8992c9474f5a62695a9775e13d080b
Timon Loeffen <timon.loeffen@aixigo.com>
2020-01-21 13:38:33 +0100

    basic eslint, vscode settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
+   root: true,
+   env: {
+      node: true
+   },
+   extends: [
+      'plugin:vue/essential',
+      'laxarjs'
+   ],
+   rules: {
+      'no-console': 'error',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'max-len': [ 'error', { code: 120 } ],
+      'space-in-parens': [ 'error', 'always' ],
+      'object-curly-spacing': [ 'error', 'always' ],
+      'array-bracket-spacing': [ 'error', 'always' ]
+   },
+   parserOptions: {
+      parser: 'babel-eslint'
+   },
+   overrides: [
+      {
+         files: [
+            '**/__tests__/*.{j,t}s?(x)'
+         ],
+         env: {
+            jest: true
+         }
       }
-    }
-  ]
-}
+   ]
+};
